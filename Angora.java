5556ebcd11c2a884f2f68b59a3d6a8720060d56e
ygazi16 <57335281+ygazi16@users.noreply.github.com>
2020-03-01 05:25:47 +0300

    Add files via upload
    Grade 100.0/100

---
+++ Angora.java
@@ -0,0 +1,18 @@
+package code;
+
+/* Modify this file using the following info
+ * - Angora is a Cat breed  
+ * - Angora is not considered a wild cat
+ * - Angora moves with 15 units of speed
+ * - Angora makes a "meow" sound
+ * - Angora cats may have differently colored eyes  (Hint: add this to its description)
+ * 
+ * */
+
+public class Angora {
+/*
+ * 
+ *   YOUR CODE HERE
+ * 
+ * */
+}
---
+++ Caracal.java
@@ -0,0 +1,15 @@
+package code;
+
+/* Modify this file using the following info
+ * - Caracal is a Cat breed  
+ * - Caracal is considered a wild cat
+ * - Caracal moves with 22 units of speed
+ * - Caracal makes a "hiss" sound
+ * - Caracal cats have longer ears (Hint: add this to its description)
+ * 
+ * */
+
+
+public class Caracal {
+
+}
